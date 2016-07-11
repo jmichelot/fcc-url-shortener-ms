45fa32a7acd776aea081aee8122f2b9e8ab36195
Julien Michelot <jmichelot@yahoo.com>
2016-07-11 15:40:59 +0200

    MongoDB ready

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 var express = require('express');
 var app = express();
-
+var mongo = require('mongodb');
+var config;
+try { config = require('./config.js'); }
+catch (err) { console.log('config.js not found.'); }
+var mongoUrl = process.env.MONGODB_URI || config.mongoUri;
 var port = process.env.PORT || 3000;
 
-app.get('/', function (req, res) {
-  res.send('Hello World');
-});
+mongo.connect(mongoUrl, function (err, db) {
+  if (err) throw new Error('Database failed to connect!');
+  else console.log('Successfully connected to MongoDB.');
 
-app.listen(port, function () {
-  console.log('Node.js listening on port ' + port + '...');
+  app.get('/', function (req, res) {
+    res.send('Hello World');
+  });
+
+  app.listen(port, function () {
+    console.log('Node.js listening on port ' + port + '...');
+  });
 });
